7ac7abe79776f60a09e298797f0cea9324e5a060
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-21 03:12:57 +0900

    feat : mission1

---
+++ src/test/java/org/prgrms/devcourse/springbootsecuritymission/SpringbootSecurityMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgrms.devcourse.springbootsecuritymission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
