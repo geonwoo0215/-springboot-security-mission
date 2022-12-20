0a6e835c496c8a6a32ed3a3dda7cc3d7af770522
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2022-12-21 04:51:21 +0900

    feat : mission2

--- src/main/java/org/prgrms/devcourse/springbootsecuritymission/configures/WebSecurityConfigure.java
+++ src/main/java/org/prgrms/devcourse/springbootsecuritymission/configures/WebSecurityConfigure.java
@@ -1,11 +1,22 @@
 package org.prgrms.devcourse.springbootsecuritymission.configures;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -38,21 +49,56 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 			.antMatchers("/me").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated()")
 			.anyRequest().permitAll()
+
 			.and()
+
 			.formLogin()
 			.defaultSuccessUrl("/")
 			.permitAll()
+
 			.and()
+
 			.logout()
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 			.invalidateHttpSession(true)
 			.clearAuthentication(true)
+
 			.and()
+
 			.rememberMe()
 			.rememberMeParameter("remember-me")
 			.tokenValiditySeconds(300)
+
+			.and()
+
+			.requiresChannel()
+			.antMatchers().requiresSecure()
+
+			.and()
+
+			.exceptionHandling()
+			.accessDeniedHandler(accessDeniedHandler())
+
 		;
+
 	}
+
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		return (request, response, e) -> {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Object principal = authentication != null ? authentication.getPrincipal() : null;
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			response.setContentType("text/plain");
+			response.getWriter().write("ACCESS DENIED ##");
+			response.getWriter().flush();
+			response.getWriter().close();
+
+		};
+
+	}
+
 
 }
